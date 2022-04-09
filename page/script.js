336b43fd3d002d798f431c4d33419fc40b92437e
Yoogy Handi Nata <72843811+handinatayoogy@users.noreply.github.com>
2022-04-10 00:43:35 +0700

    validasi

--- page/script.js
+++ page/script.js
@@ -1,13 +1,20 @@
+let item;
+let wrapper;
+
 function allowDrop(ev) {
   ev.preventDefault();
 }
 
 function drag(ev) {
+  item = ev.path[0].className.split(" ")[1];
   ev.dataTransfer.setData("text", ev.target.id);
 }
 
 function drop(ev) {
-  ev.preventDefault();
-  let data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data));
+  wrapper = ev.target.className.split(" ")[2];
+  if (item === wrapper) {
+    ev.preventDefault();
+    let data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
 }
