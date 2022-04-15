f811c67bc06342e28a58aa3a21b3e2db56a7c511
Yoogy Handi Nata <72843811+handinatayoogy@users.noreply.github.com>
2022-04-16 01:55:32 +0700

    fix responsive

--- index.php
+++ index.php
@@ -14,12 +14,14 @@ header("Access-Control-Allow-Headers: X-Requested-With");
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
 <link rel='stylesheet' href='https://sarasoueidan.com/demos/windows8-animations/css/demo-styles.css'><link rel="stylesheet" href="./style.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
 
 </head>
 <body>
 <!-- partial:index.partial.html -->
-<div class="demo-wrapper">
+<div class="demo-wrapper container">
 <!-- classnames for the pages should include: 1) type of page 2) page name-->
   <div class="s-page random-restored-page">
     <div class="page-content">
@@ -142,6 +144,7 @@ header("Access-Control-Allow-Headers: X-Requested-With");
 </div>
 <!-- partial -->
   <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-<script src='https://sarasoueidan.com/demos/windows8-animations/js/scripts.js'></script>
+  <script src='https://sarasoueidan.com/demos/windows8-animations/js/scripts.js'></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
